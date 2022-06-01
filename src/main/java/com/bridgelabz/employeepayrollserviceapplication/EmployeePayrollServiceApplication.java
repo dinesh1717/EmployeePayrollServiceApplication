a18229871dfe9b886277f63cbe7107090452ec1b
dinesh nangare <dineshnangare1717@gmail.com>
2022-06-02 00:13:20 +0530

    creating employee service application

---
+++ src/main/java/com/bridgelabz/employeepayrollserviceapplication/EmployeePayrollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollserviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee PayRoll Service Application");
+        SpringApplication.run(EmployeePayrollServiceApplication.class, args);
+    }
+}
